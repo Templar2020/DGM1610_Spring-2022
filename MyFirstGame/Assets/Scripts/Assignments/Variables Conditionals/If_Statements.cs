ee556b50795905541aaadce1d05c3d6915c326e8
Preston Powell <preston.powell2020@gmail.com>
2022-01-26 17:13:42 -0700

    IF Statements. Setup Prototype One.

---
+++ MyFirstGame/Assets/Scripts/Assignments/Variables Conditionals/If_Statements.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If_Statements : MonoBehaviour
+{
+    // public string lightColor;
+
+    //public string weather;
+
+    public string hairColor;
+
+    public int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+       /*
+       if(lightColor == "Red")
+       {
+           Debug.Log("The light is "+ lightColor +" STOP!!!!");
+       }
+       else if(lightColor == "Yellow")
+       {
+           Debug.Log("The light is "+ lightColor +" start to Stop or Clear the intersection!");
+       }
+       else if(lightColor == "Green")
+       {
+           Debug.Log("The light is "+ lightColor +" GO GO GO!");
+       }
+       else
+       {
+           Debug.Log("Wrong color!");  
+       }
+       */
+        /* 
+       if(weather == "Rain" || weather == "Snow")
+       {
+           Debug.Log("The weather is bad, you should wear a coat!");
+       }
+       else if(weather == "Sunny"|| weather == "Partly Cloudy")
+       {
+           Debug.Log("You do not need a coat!");
+       }
+       */
+        if(hairColor == "Red" && age == 6)
+        {
+            Debug.Log("You can ride the Merry Go Round!");
+        }
+        else if(hairColor == "Blonde" && age == 100)
+        {
+           Debug.Log("You are old enough to ride the Fire Dragon ride!!!!"); 
+        }
+
+       
+    }
+}
